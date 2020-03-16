537e1a199bcdd309b034df353eae94be31fdccb8
unknown <C:\Users\User\AppData\Roaming\The Bat!>
2020-03-16 13:35:01 +0300

    docs: update all

--- script.js
+++ script.js
@@ -4,3 +4,44 @@ MENU.addEventListener('click', (event) => {
     MENU.querySelectorAll('li').forEach(el => el.classList.remove('active'));
     event.target.classList.add('active');
 })
+
+const MODELV = document.getElementById('iPhone-V')
+MODELV.addEventListener('click', function () {
+    if (document.getElementById('pic-V').hidden == true) document.getElementById('pic-V').hidden = false;
+        else document.getElementById('pic-V').hidden = true;
+}, false);
+
+const MODELH = document.getElementById('iPhone-H')
+MODELH.addEventListener('click', function () {
+    if (document.getElementById('pic-H').hidden == true) document.getElementById('pic-H').hidden = false;
+    else document.getElementById('pic-H').hidden = true;
+}, false);
+
+//const CHANGE = document.getElementById('prev-slide')
+//CHANGE.addEventListener('click', function () {
+//    CHANGE.querySelectorAll('Slider').
+//};
+
+const CHANGEP = document.getElementById('prev-slide')
+CHANGEP.addEventListener('click', function () {
+    if (document.getElementById('Slide-1').hidden == true) {
+        document.getElementById('Slide-1').hidden = false;
+        document.getElementById('Slide-2').hidden = true;
+    }
+    else {
+        document.getElementById('Slide-1').hidden = true;
+        document.getElementById('Slide-2').hidden = false;
+    }
+}, false);
+
+const CHANGEN = document.getElementById('next-slide')
+CHANGEN.addEventListener('click', function () {
+    if (document.getElementById('Slide-1').hidden == true) {
+        document.getElementById('Slide-1').hidden = false;
+        document.getElementById('Slide-2').hidden = true;
+    }
+    else {
+        document.getElementById('Slide-1').hidden = true;
+        document.getElementById('Slide-2').hidden = false;
+    }
+}, false);
