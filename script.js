298317f087fccceb4fcb979153139a993a560e70
unknown <C:\Users\User\AppData\Roaming\The Bat!>
2020-03-16 10:10:08 +0300

    docs: update index and script

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 const MENU=document.getElementById('menu');
 
-MENU.querySelectorAll('li');
-
 MENU.addEventListener('click', (event) => {
     MENU.addEventListener('li').forEach(el => el.classList.remove('active'));
     event.target.clasList.add('active');
